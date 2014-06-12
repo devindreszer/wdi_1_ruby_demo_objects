92e433b6f63ab9cb3544fc353231e03ba364103f
Devin Dreszer <devindreszer@gmail.com>
2014-06-12 15:56:13 -0400

    Add a song class

--- bin/song_app.rb
+++ bin/song_app.rb
@@ -4,3 +4,50 @@ require_relative '../lib/song.rb'
 
 # Add code to use the Song class here.
 
+video_games = Song.new("Video Games", "Lana Del Rey", 282, 1.00, '01-01-2012')
+happy = Song.new("Happy", "Pharrell", 233, 1.25, '01-01-2014')
+bad_kids = Song.new("The Black Lips", "Bad Kids", 128, 0.75, '01-01-2007')
+
+playlist = [video_games, happy, bad_kids]
+
+def puts_all(playlist)
+  playlist.each do |song|
+    puts song.show
+  end
+end
+
+def average_duration(playlist)
+  total_duration = 0
+
+  playlist.each do |song|
+    total_duration += song.duration
+  end
+
+  average_duration = total_duration / playlist.length
+end
+
+def average_price(playlist)
+  total_price = 0
+
+  playlist.each do |song|
+    total_price += song.price
+  end
+
+  average_price = total_price / playlist.length
+end
+
+puts_all(playlist)
+puts
+puts "The average song duration is #{average_duration(playlist)} seconds."
+puts "The average song price is #{average_price(playlist)} dollars."
+
+happy.price = 5
+
+puts
+puts "The price changed!"
+puts
+puts "The average song duration is #{average_duration(playlist)} seconds."
+puts "The average song price is #{average_price(playlist)} dollars."
+
+puts bad_kids.years_since_release
+puts bad_kids.artist_percentage
---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+require 'date'
+
+class Song
+  attr_reader :title, :artist_name, :duration, :release_date, :artist_percentage
+  attr_accessor :price
+
+  def initialize(title, artist_name, duration, price, release_str)
+    @title = title
+    @artist_name = artist_name
+    @duration = duration
+    @price = price
+    @release_date = Date.strptime(release_str, '%m-%d-%Y')
+    @artist_percentage = [0.20 - 0.05 * (years_since_release / 5).floor, 0].max
+  end
+
+  def years_since_release
+    Date.today.year - @release_date.year
+  end
+
+  def show
+    "#{@title} by #{artist_name}"
+  end
+
+end
+
